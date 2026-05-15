7e27e1765c97253d233cea713a462cc4ba1961db
Mahliyo <gasandigital13@gmail.com>
2026-05-15 16:42:38 +0500

    react start i18n ru uz en

--- src/Component/Cards/Cards.jsx
+++ src/Component/Cards/Cards.jsx
@@ -1,10 +1,13 @@
+import { useTranslation } from "react-i18next";
 import "./Cards.css";
 import React from 'react'
 
 function Cards() {
-    const {t, }
+    const {t, i18n } = useTranslation();
   return (
-    <div>Cards</div>
+    <div className="container">
+        <h1 className="title"></h1>
+    </div>
   )
 }
 
